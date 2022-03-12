3dba6912fee3e3d6ff1383389dc7645a45b5bfec
KristianvK <56249565+KristianvK@users.noreply.github.com>
2022-03-12 15:15:51 +0100

    button working

--- MyApp.py
+++ MyApp.py
@@ -14,6 +14,7 @@ class MyApp:
         self.frame = Frame(self.window, bg='#e28743')
 
         # creation des composants
+        self.profit = tk.IntVar()
         self.create_widgets()
 
         # empaquetage
@@ -24,13 +25,13 @@ class MyApp:
 
     def create_grid(self):
         # entries
-        time_in_bed = tk.Entry(master=self.frame, fg="white", bg="#e28743", width=20, font = ('Helvetica', 15))
+        self.time_in_bed = tk.Entry(master=self.frame, fg="white", bg="#e28743", width=20, font = ('Helvetica', 15))
         time_go_bed = tk.Entry(master=self.frame, fg="white", bg="#e28743", width=20, font = ('Helvetica', 15))
         time_wake_up = tk.Entry(master=self.frame, fg="white", bg="#e28743", width=20, font = ('Helvetica', 15))
         steps = tk.Entry(master=self.frame, fg="white", bg="#e28743", width=20, font = ('Helvetica', 15))
 
         # labels
-        lbl_title = tk.Label(master=self.frame, text="Please input your details", font=("Helvetica", 40), bg='#e28743', fg='white')
+        self.lbl_title = tk.Label(master=self.frame, text="Please input your details", font=("Helvetica", 40), bg='#e28743', fg='white')
         empty_text = tk.Label(master=self.frame, text="", bg='#e28743', fg='#e28743')
         lbl_time_in_bed = tk.Label(master=self.frame, text="time in bed", font=("Helvetica", 25), bg='#e28743', fg='white')
         lbl_time_go_to_bed = tk.Label(master=self.frame, text="start of sleep", font=("Helvetica", 25), bg='#e28743', fg='white',)
@@ -38,21 +39,22 @@ class MyApp:
         lbl_steps = tk.Label(master=self.frame, text="number of steps", font=("Helvetica", 25), bg='#e28743', fg='white')
 
         # button
-        button = Button(self.frame, text="Get my Sleep Score!", font=("Helvetica", 25), bg='white', fg='#e28743',
-                           # command=
-                           )
+        button = Button(self.frame, text="Get my Sleep Score!", font=("Helvetica", 25), bg='white', fg='#e28743', command=lambda: self.increase())
 
         # grid of the frame
         self.frame.grid(row=0, column=0, padx=10)
 
+        # labelProfit = tk.Label(self.frame, textvariable=self.profit)
+        # labelProfit.grid(row=7, column=0)
+
         # position of entries
-        time_in_bed.grid(row=2, column=1, padx=10,pady=10)
+        self.time_in_bed.grid(row=2, column=1, padx=10, pady=10)
         time_go_bed.grid(row=3, column=1,pady=10)
         time_wake_up.grid(row=4, column=1,pady=10)
         steps.grid(row=5, column=1,pady=10)
 
         # position of labels
-        lbl_title.grid(row=0, column=0, columnspan=2)
+        self.lbl_title.grid(row=0, column=0, columnspan=2)
         empty_text.grid(row=1,column=0, columnspan=2)
         lbl_time_in_bed.grid(row=2, column=0, sticky="w")
         lbl_time_go_to_bed.grid(row=3, column=0, sticky="w")
@@ -60,6 +62,10 @@ class MyApp:
         lbl_steps.grid(row=5, column=0, sticky="w")
 
         button.grid(row=6, column=0, columnspan=2)
+
+    def increase(self):
+        self.lbl_title['text'] = "Something else"
+
 
 # afficher
 app = MyApp()
