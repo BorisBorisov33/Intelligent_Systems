7708ac1431fc18b49eeff3877dc1c07db421803d
KristianvK <56249565+KristianvK@users.noreply.github.com>
2022-03-12 13:36:53 +0100

    removed pack and added grid

--- MyApp.py
+++ MyApp.py
@@ -8,7 +8,7 @@ class MyApp:
         self.window.title("My Application")
         self.window.geometry("960x540")
         self.window.minsize(480, 360)
-        self.window.iconbitmap("logo.ico")
+        # self.window.iconbitmap("logo.ico")
         self.window.config(background='#e28743')
         # initialization des composants
         self.frame = Frame(self.window, bg='#e28743')
@@ -22,31 +22,57 @@ class MyApp:
     def create_widgets(self):
         self.create_title()
         self.create_subtitle()
-        self.create_textbox()
+        self.create_grid()
         self.create_youtube_button()
 
-    def create_textbox(self):
-        text = Label(self.frame, text="Sleep Time", font=("Helvetica", 40), bg='#e28743',
-                            fg='white')
-        text.pack()
-        textBox = Text(self.frame, height=2, width=10).place(x=300, y=400)
-        # textBox.pack(side=self.root.RIGHT)
+    def create_grid(self):
+        # entries
+        time_in_bed = tk.Entry(master=self.frame, fg="white", bg="blue", width=50)
+        time_go_bed = tk.Entry(master=self.frame, fg="white", bg="blue", width=50)
+        time_wake_up = tk.Entry(master=self.frame, fg="white", bg="blue", width=50)
+        steps = tk.Entry(master=self.frame, fg="white", bg="blue", width=50)
+
+        # labels
+        lbl_title = tk.Label(master=self.frame, text="Please input your details")
+        lbl_time_in_bed = tk.Label(master=self.frame, text="time in bed")
+        lbl_time_go_to_bed = tk.Label(master=self.frame, text="start of sleep")
+        lbl_wake_up = tk.Label(master=self.frame, text="end of sleep")
+        lbl_steps = tk.Label(master=self.frame, text="number of steps")
+
+        # button
+        btn_enter = tk.Button(master=self.frame, text="Enter")
+
+        # grid of the frame
+        self.frame.grid(row=0, column=0, padx=10,pady=10)
+
+        # position of entries
+        time_in_bed.grid(row=1, column=0, padx=10,pady=10)
+        time_go_bed.grid(row=2, column=0,pady=10)
+        time_wake_up.grid(row=3, column=0,pady=10)
+        steps.grid(row=4, column=0,pady=10)
+
+        # position of labels
+        lbl_title.grid(row=0, column=1)
+        lbl_time_in_bed.grid(row=1, column=1, sticky="w")
+        lbl_time_go_to_bed.grid(row=2, column=1, sticky="w")
+        lbl_wake_up.grid(row=3, column=1, sticky="w")
+        lbl_steps.grid(row=4, column=1, sticky="w")
 
     def create_title(self):
         label_title = Label(self.frame, text="Welcome", font=("Helvetica", 40), bg='#e28743',
                             fg='white')
-        label_title.pack()
+        # label_title.pack()
 
     def create_subtitle(self):
         label_subtitle = Label(self.frame, text="Hey guys", font=("Helvetica", 25), bg='#e28743',
                                fg='white')
-        label_subtitle.pack()
+        # label_subtitle.pack()
 
     def create_youtube_button(self):
         yt_button = Button(self.frame, text="Run", font=("Helvetica", 25), bg='white', fg='#e28743',
                            # command=
                            )
-        yt_button.pack(pady=25, fill=X)
+        # yt_button.pack(pady=25, fill=X)
 
 # afficher
 app = MyApp()
